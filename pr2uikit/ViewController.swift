24feaedc17cabcdd269cf613596ccf032b40ba5b
Mikita <nikita_popow_1993@mail.ru>
2023-09-06 13:21:59 +0300

    Initial Commit

---
+++ pr2uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pr2uikit
+//
+//  Created by Никита Попов on 6.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
